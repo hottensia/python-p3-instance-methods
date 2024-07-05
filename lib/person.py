4da38b54a5ab593731ecddeb6a8016b57d915c75
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-07-05 07:09:34 +0300

    First commit

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,17 @@
 
 class Person:
     # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+
+      #Instance method definition
+
+alice = Person()
+alice.talk()
+alice.walk()
+
+
+  
